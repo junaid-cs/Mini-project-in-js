79fc54943dd6139fda11100c9815044cebdfccb0
Jay <85805386+Jay-khan421@users.noreply.github.com>
2023-01-23 17:44:06 +0500

    working in navbar

--- ecommerce_react_app/src/App.js
+++ ecommerce_react_app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home'
 import About from './pages/About'
 const App = () => (
     <>
-    <Nav />
-    <Home />
-    <About />
+    <Router>
+        <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+  </Router>
     </>
 )
 
--- ecommerce_react_app/src/components/Nav.js
+++ ecommerce_react_app/src/components/Nav.js
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom';
 const Nav = () => {
-  return  <h1>Hi I am nav</h1>
+  return  (
+    <>
+     <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    </>
+  )
 }
 
 export default Nav
