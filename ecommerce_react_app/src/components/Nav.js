db68a70bf91b828a8b4e2e77ed78ba53d0af2a5e
Jay <85805386+Jay-khan421@users.noreply.github.com>
2023-01-24 07:49:29 +0500

    nav

--- ecommerce_react_app/src/components/Nav.js
+++ ecommerce_react_app/src/components/Nav.js
@@ -1,9 +1,21 @@
 import {Link} from 'react-router-dom';
+import './Nav.css';
 const Nav = () => {
   return  (
     <>
-     <Link to='/'>Home</Link>
+    <nav className='container d-flex justify-content-between p-1'>
+    
+    <Link to='/'>  <h2>Logo</h2></Link>
+    <ul className='d-flex'>
+      <li className='p-2'>
+    <Link to='/'>Home</Link>
+      </li>
+      <li className='p-2'>
     <Link to='/about'>About</Link>
+      </li>
+
+    </ul>
+    </nav>
     </>
   )
 }
