2b7f5a3fa1321c4982a7ae37307a8cf580c74baf
Jay <85805386+Jay-khan421@users.noreply.github.com>
2023-01-27 20:45:55 +0500

    cart style

--- ecommerce_react_app/src/components/Nav.js
+++ ecommerce_react_app/src/components/Nav.js
@@ -15,7 +15,10 @@ const Nav = () => {
     <Link to='/products'>Products</Link>
       </li>
       <li className='p-2 nav_link'>
-    <Link to='/caret'>Caret</Link>
+    <Link to='/caret' style={{backgroundColor:'yellow'} } className="px-2 py-1 rounded-4">
+      <span className='mx-1 fw-bold' style={{fontSize:14}}>3</span>
+      <img src="/images/logo.png" style={{width:22}} alt="" srcset="" />
+    </Link>
       </li>
 
     </ul>
